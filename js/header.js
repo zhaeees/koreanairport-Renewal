ba1d9efb05b4459018fc4099ef56cb0e982f791c
zhaeees <yellowpado@gmail.com>
2026-03-04 11:18:40 +0900

    팝업변경

--- js/header.js
+++ js/header.js
@@ -231,12 +231,18 @@ document.addEventListener("DOMContentLoaded", () => {
     popup.classList.add("active");
     document.body.style.overflow = "hidden";
   }
-  // 팝업 닫기 함수
-    function closePopup() {
-      openpopup.classList.remove("active");
-      document.body.style.overflow = ""; // 스크롤 복원
-    }
-// 처음 로드 시 자동 오픈
+
+  function closePopup() {
+    if (!popup) return;
+    popup.classList.remove("active");
+    document.body.style.overflow = "";
+
+    
+    sessionStorage.setItem(popupKey, "true");
+  }
+
+  // 메인이고 + 아직 안 닫았을 때만 열기
+  if (isMainPage && !sessionStorage.getItem(popupKey)) {
     openPopup();
   }
 
